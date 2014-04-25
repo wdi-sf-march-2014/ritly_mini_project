db008021ba01cc82b338bd1f09dc81ed07d7c51d
Sterling Radcliff <radcliff@riseup.net>
2014-04-25 12:36:20 -0700

    most of controller, some progress on views

--- ritly/app/controllers/urls_controller.rb
+++ ritly/app/controllers/urls_controller.rb
@@ -8,5 +8,25 @@ class UrlsController < ApplicationController
 	end
 
 	def create
-		params[:url][:link]
+		@url = Url.create url_params
+		redirect_to url_path(@url)
+	end
+
+	def show
+		@url = Url.find(params[:id])
+	end
+
+	def edit
+		@url = Url.find(params[:id])
+	end
+
+	def update
+		@url = Url.update url_params
+		redirect_to url_path(@url)
+	end
+
+private
+	def url_params
+		params.require(:url).permit(:link, :random_string)
+	end
 end
