ebee638fa46b8ef295e768eed5fda16d6daec484
blairprophett <blaircuny@gmail.com>
2014-04-24 22:43:35 -0700

    first commit, paths partially setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RitlyApp::Application.config.secret_key_base = '50b189016c4484768cb63a2b9b4f754ed9ebfbee7e6018e38a2bbe25cdfea04789550a44388fb5e3f3c4e89f3019d068e06ff765f596bbeff278d4036c14c117'
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+urls = Url.create([
+	{link: "http://www.generalassemb.ly", random_string: "m1gpLHq8aoY"},
+
+	{link: "http://www.google.com", random_string: "T7DViXiIxFE"},
+
+	{link: "http://www.bustedtees.com", random_string: "pxMpvdB8LRw"}
+
+	])
