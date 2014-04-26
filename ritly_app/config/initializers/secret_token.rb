8947304aaa5bd9cf49b0096636fe71c0cb7bc72d
taylorchiu <taylorparkerchiu@gmail.com>
2014-04-25 17:26:23 -0700

    Working App

---
+++ ritly_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RitlyApp::Application.config.secret_key_base = 'ec7f9ef0ff5526a129bb6ae4fff8d9494497f0c8e640a444cbdefd24b1782b9571765a16258775cfa41a48caf46cca53d1c9a9313dff16f6d039c9c0189208b9'
---
+++ ritly_app/db/seeds.rb
@@ -0,0 +1,8 @@
+urls = Url.create([
+  {link: "www.google.com", random_string: "1agvhY7"},
+
+  {link: "www.cat-bounce.com", random_string: "1g0xK6B"},
+
+  {link: "http://ducksarethebest.com", random_string: "IFHzvO"},
+
+   ])
