66136bf882db8aaceb601bfca9d680c6005e5ebb
blairprophett <blaircuny@gmail.com>
2014-04-26 13:56:14 -0700

    controllers rspec, some CSS

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
-describe Url do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'link' do
+	it 'should not be empty' do
+		url = Url.create(link: "http://www.google.com")
+		url.should_not be_valid
+		url.errors[:link].should include("can't be blank")
+	end
+	
 end
