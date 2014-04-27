ec76576d4d346640e3aa9309c7f007a4e86a0fb2
Randy Carlisle <cisolutionsinc@gmail.com>
2014-04-27 00:33:27 -0700

    created initial db records

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,11 +2,16 @@ class UrlsController < ApplicationController
 
 	def index
 		@url = Url.new
+		#@all_widgets = Widget.all
 	end 
 
 	def create
 		# expecting @url object from index.html.erb
 		# in order to create new Url row with params hash
+		token = SecureRandom.urlsafe_base64(3)
+		new_url = Url.create(link: params[:url][:link], random_string: token)
+	
+		render "/"
 	end
 
 	def destroy
