f0e9e442209b8d855e08ff389bbd35ae90203488
Githubshanna <shannahutch@gmail.com>
2014-04-28 09:02:10 -0700

    Ritly app with rspec

--- ritly_app/spec/controllers/urls_controller_spec.rb
+++ ritly_app/spec/controllers/urls_controller_spec.rb
@@ -1,51 +1,60 @@
 require 'spec_helper'
 # 200 status check
-describe "Urls Controller" do
-
-	# 200 status check
-
 	
   
 
-  describe 'GET new' do
+  
+describe UrlsController do
 
-    before(:each) do
-      @url = Url.create(link: "wwww.gmail.com", random_string: "qwertyui" )
-    end
+  before(:each) do
+  @url = Url.create(link: "wwww.google.com", random_string: "ABCDEFG" )
+  end
 
+  describe 'Get new' do
     it 'returns a 200 Ok status' do
-      #url = Url.create
-      get :index
-      response.should be_ok
+      url = Url.create
+      get :new
+      expect(response.status).to eq(200)
     end
 
     it 'renders the new template' do
-  	 get :new
-  	 expect(response).to render_template("new")
-    end
-
-    it 'renders the show template' do
-     get :show, id: @url.id
+     get :new
      expect(response).to render_template("new")
     end
-
   end
 
+  describe 'Get index' do
+    it 'returns a 200 Ok status' do
+      url = Url.create
+      get :index
+      expect(response.status).to eq(200)
+    end
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template("index")  
+  end
+  end
+  
+  describe 'Get edit' do
+    it 'returns a 200 Ok status' do
+      get :edit, id:@url.id 
+      expect(response.status).to eq(200)
+    end
+    it 'renders the edit template' do
+      get :edit, id:@url.id
+      expect(response).to render_template("edit")
+    end
+    end  
+      
+  describe 'Get show' do
+    it 'returns a 20 OK status' do
+      get :show, id:@url.id
+      expect(response.status).to eq(200)
+    end   
+    it 'renders the show template' do
+      get :show, id:@url.id
+      expect(response).to render_template("show")
+    end   
+  end
 end
-# 	describe 'creating a new url' do
-# 		it 'renders a form and creates a new url' do
-		
-# 		get '/urls/new'
-# 		expect(response).to render_template(:new)
 
-# 		post "/urls", urls: {link: "This is your New Unique URL"}
-
-# 		expect(response.status).to be_success 
-# 		follow_redirect!
-
-# 		expect(response).to render_template(:show)
-# 		expect(response.body).to include("This is your New Unique URL")
-
-# 		end
-# 	end
-# end
