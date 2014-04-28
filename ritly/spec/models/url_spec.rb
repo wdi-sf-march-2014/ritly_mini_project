7cb2015629f61a432036a00e2dcb1becbdeff4da
shane <shanemiller500@gmail.com>
2014-04-28 09:21:54 -0700

    finished

--- ritly/spec/models/url_spec.rb
+++ ritly/spec/models/url_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe Url do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should not be empty' do
+    url = Url.create(random_string: "12edr")
+    url.should_not be_valid
+    url.errors[:link].should include("can not be blank")
+  end
 
-  
+  it 'is at least two characters' do
+    url = Url.create(link: "www.facebook.com", random_string: "23j5t")
+    url.should be_valid
+  end
 end
